72fcf27216f148efef9b9d2c019303c4de6fcad3
tom1299 <tom1299@jeeatwork.com>
2026-09-19 09:54:48 +0200

    Checkpoint before switching to pycharm

--- agents/git-agent/src/git_agent/commit_classification_agent.py
+++ agents/git-agent/src/git_agent/commit_classification_agent.py
@@ -10,15 +10,15 @@ def create_agent(middleware: list, tools: list, checkpointer=None):
         checkpointer=checkpointer,
         response_format=Classification,
         middleware=middleware,
-        system_prompt="You receive a Change object representing a commit in a git repository."
-                      "It contains the commit hash, date, changes (diff), subject, and commit message."
-                      "Your task is to classify the commit based on"
-                      "the semantic impact on the overall document / file changed and it size"
-                      "Return a classification object with the fields size and semantic_impact set."
-                      "Also add a brief explanation (10-20 words) for the classification in the reason field."
-                      "You receive the path to the git repository as well. Always use it to read the file content before and after the commit"
-                      "to determine the size and semantic impact of the change. Use tools get_content_before_commit and get_content_after_commit for that"
-                      "Only use these tools once per commit."
+        # system_prompt="You receive a Change object representing a commit in a git repository."
+        #               "It contains the commit hash, date, changes (diff), subject, and commit message."
+        #               "Your task is to classify the commit based on"
+        #               "the semantic impact on the overall document / file changed and it size"
+        #               "Return a classification object with the fields size and semantic_impact set."
+        #               "Also add a brief explanation (10-20 words) for the classification in the reason field."
+        #               "You receive the path to the git repository as well. Always use it to read the file content before and after the commit"
+        #               "to determine the size and semantic impact of the change. Use tools get_content_before_commit and get_content_after_commit for that"
+        #               "Only use these tools once per commit."
     )
     return agent_with_middleware
 
--- agents/git-agent/tests/test_commit_classification_agent.py
+++ agents/git-agent/tests/test_commit_classification_agent.py
@@ -6,6 +6,7 @@ import unittest
 
 from pathlib import Path
 from typing import TypedDict, Annotated, NotRequired
+from unittest import skip
 
 from langchain.agents import AgentState
 from langchain.agents.middleware import wrap_tool_call
@@ -13,6 +14,7 @@ from langchain_core.messages import ToolMessage
 from langchain.tools.tool_node import ToolCallRequest
 from langgraph.checkpoint.memory import InMemorySaver
 from langgraph.types import Command
+from openai.types import skill_create_params
 
 from agent_tools.git import add_changes, ChangeHistory, get_content_after_commit, get_content_before_commit
 from git_agent.commit_classification_agent import create_agent
@@ -32,17 +34,39 @@ class TrackingState(AgentState):
 @wrap_tool_call(state_schema=TrackingState)
 def tool_invocation_counter_middleware(request: ToolCallRequest, handler) -> ToolMessage | Command:
     result = handler(request)
-    count = request.state.get("tool_call_count", 0) + 1
-    return Command(
-        update={
-            # Preserve the tool result in the agent message history.
-            "messages": [result],
-            "tool_call_count": count
-        }
-    )
+    return result
+    # count = request.state.get("tool_call_count", 0) + 1
+    # return Command(
+    #     update={
+    #         # Preserve the tool result in the agent message history.
+    #         "messages": [result],
+    #         "tool_call_count": count
+    #     }
+    # )
 
 class TestCommitClassificationAgent(unittest.TestCase):
 
+    def test_classify_commit_without_change(self):
+        # TODO: Examine why endless loop on tool invocation if get before and after commit
+        # when using model openai:gpt-4o-mini
+        inMemoryCheckPointer = InMemorySaver()
+        config = {
+            "configurable": {
+                "thread_id": "us-weather"
+            }
+        }
+
+        middleware = [tool_invocation_counter_middleware]
+        tools = [get_content_before_commit, get_content_after_commit]
+        agent = create_agent(middleware=middleware, tools=tools, checkpointer=inMemoryCheckPointer)
+
+        # result = agent.invoke(message, config=config)
+        result = agent.invoke({"messages": [{"role": "user", "content": "Call tool get_content_before_commit and get_content_after_commit for commit '90d449e0c3fa65cdcf61dac336121f5586644157', file 'content/en/docs/concepts/services-networking/service.md' in the repo path at " + str(TEST_DATA_DIR) + " and analyse the changes"}]}, config=config)
+
+        classified_change = result["structured_response"]
+        print(f"Classified change for commit 90d449e0c3fa65cdcf61dac336121f5586644157: {classified_change}, {classified_change.reason}")
+
+    @skip
     def test_classify_commit(self):
         repo_path = TEST_DATA_DIR
         since = "1 year ago"
@@ -57,28 +81,29 @@ class TestCommitClassificationAgent(unittest.TestCase):
             }
         }
 
-        for i in range(1):
-            change = change_history.changes[i]
-            content_after_commit = get_content_after_commit.func(commit_hash=change.commit_hash, repo_path=str(repo_path), file_path=file_path)
-            message = {
-                "messages": [
-                    {
-                        "role": "user",
-                        "content": json.dumps(
-                            {
-                                "change": change.model_dump(mode="json"),
-                                "repo_path": str(repo_path)
-                            }
-                        )
-                    }
-                ]
-            }
+        change = change_history.changes[0]
+        content_after_commit = get_content_after_commit.func(commit_hash=change.commit_hash, repo_path=str(repo_path), file_path=file_path)
+        message = {
+            "messages": [
+                {
+                    "role": "user",
+                    "content": json.dumps(
+                        {
+                            "change": change.model_dump(mode="json"),
+                            "repo_path": str(repo_path)
+                        }
+                    )
+                }
+            ]
+        }
 
-            # TODO: Examine why endless loop on tool invocation if get before and after commit content is fetched
-            middleware = [tool_invocation_counter_middleware]
-            tools = [get_content_before_commit, get_content_after_commit]
-            agent = create_agent(middleware=middleware, tools=tools, checkpointer=inMemoryCheckPointer)
+        # TODO: Examine why endless loop on tool invocation if get before and after commit content is fetched
+        middleware = [tool_invocation_counter_middleware]
+        tools = [get_content_before_commit, get_content_after_commit]
+        agent = create_agent(middleware=middleware, tools=tools, checkpointer=inMemoryCheckPointer)
 
-            result = agent.invoke(message, config=config)
-            classified_change = result["structured_response"]
-            print(f"Classified change for commit {change.commit_hash}: {classified_change}, {classified_change.reason}")
+        # result = agent.invoke(message, config=config)
+        result = agent.invoke({"messages": [{"role": "user", "content": "Call tool get_content_before_commit and get_content_after_commit for commit '90d449e0c3fa65cdcf61dac336121f5586644157', file 'content/en/docs/concepts/services-networking/service.md' in the repo path at " + str(repo_path) + " and analyse the changes"}]}, config=config)
+
+        classified_change = result["structured_response"]
+        print(f"Classified change for commit {change.commit_hash}: {classified_change}, {classified_change.reason}")
